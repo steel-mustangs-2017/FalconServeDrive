d0b44f40939040364b3f3092d86ad216dcc8bbf0
Jarek Krych <jarek.krych@softwarexpressllc.com>
2023-01-14 10:53:24 -0700

    Initial code commit.

---
+++ src/main/java/frc/robot/commands/SwerveJoystickCmd.java
@@ -0,0 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.chassisConstants;
+import frc.robot.subsystems.SwerveModule;
+import frc.robot.subsystems.SwerveSubsystem;
+
+/** An example command that uses an example subsystem. */
+public class SwerveJoystickCmd extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private double rotation;
+  private Translation2d translation;
+  private boolean fieldRelative;
+  private boolean overrideJS;
+  
+  private SwerveSubsystem swerve;
+  private XboxController driver;
+  private SlewRateLimiter yLim = new SlewRateLimiter(3);
+  private SlewRateLimiter xLim = new SlewRateLimiter(1);
+  private SlewRateLimiter rotLim = new SlewRateLimiter(1);
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public SwerveJoystickCmd(SwerveSubsystem swerve, XboxController driver, boolean fieldRelative) {
+    this.swerve = swerve;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(swerve);
+
+    this.driver = driver;
+    this.fieldRelative = fieldRelative;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double yAxis = -driver.getLeftY();
+    double xAxis = -driver.getLeftX();
+    double rotAxis = -driver.getRightX();
+    if(Math.abs(rotAxis) > chassisConstants.deadband){
+      overrideJS = true;
+    }
+    else if (Math.abs(rotAxis) < chassisConstants.deadband){
+      overrideJS = false;
+    }
+
+    yAxis = (Math.abs(yAxis) < chassisConstants.deadband ? 0 : yLim.calculate(yAxis * 0.3));
+    if(overrideJS == false){
+      xAxis = (Math.abs(xAxis) < chassisConstants.deadband ? 0 : xLim.calculate(xAxis * 0.3));
+    }
+    else if(overrideJS == true){
+      xAxis = 0;
+    }
+    rotAxis = (Math.abs(rotAxis) < chassisConstants.deadband ? 0 : rotLim.calculate(rotAxis * 0.3));
+
+    translation = new Translation2d(yAxis, xAxis).times(chassisConstants.maxSpeedMPS);
+    rotation = rotAxis * chassisConstants.maxTurnSpeed;
+    swerve.drive(translation, rotAxis, fieldRelative);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,102 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.sensors.WPI_Pigeon2;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.chassisConstants;
+import frc.robot.Constants.chassisSetUp;
+
+public class SwerveSubsystem extends SubsystemBase {
+  private final SwerveModule frontLeft = new SwerveModule(chassisSetUp.fLeftDriveMotorPort, chassisSetUp.isFrontLeftDriveMotorReverse, 
+  chassisSetUp.fLeftTurnMotorPort, chassisSetUp.isFrontLeftTurnMotorReverse, chassisSetUp.fLeftAbsoluteEncoder, chassisSetUp.frontLAngle,chassisSetUp.frontLKP, chassisSetUp.frontLKI, chassisSetUp.frontLKD);
+
+  private final SwerveModule frontRight = new SwerveModule(chassisSetUp.fRightDriveMotorPort, chassisSetUp.isFrontRightDriveMotorReverse, 
+  chassisSetUp.fRightTurnMotorPort, chassisSetUp.isFrontRightTurnMotorReverse, chassisSetUp.fRightAbsoluteEncoder, chassisSetUp.frontRAngle, chassisSetUp.frontRKP, chassisSetUp.frontRKI, chassisSetUp.frontRKD);
+
+  private final SwerveModule backLeft = new SwerveModule(chassisSetUp.bLeftDriveMotorPort, chassisSetUp.isBackLeftDriveMotorReverse, 
+  chassisSetUp.bLeftTurnMotorPort, chassisSetUp.isBackLeftTurnMotorReverse, chassisSetUp.bLeftAbsoluteEncoder, chassisSetUp.backLAngle, chassisSetUp.backLKP, chassisSetUp.backLKI, chassisSetUp.backLKD);
+
+  private final SwerveModule backRight = new SwerveModule(chassisSetUp.bRightDriveMotorPort, chassisSetUp.isBackRightDriveMotorReverse, 
+  chassisSetUp.bRightTurnMotorPort, chassisSetUp.isBackRightTurnMotorReverse, chassisSetUp.bRightAbsoluteEncoder, chassisSetUp.backRAngle, chassisSetUp.backRKP, chassisSetUp.backRKI, chassisSetUp.backRKD);
+
+  private WPI_Pigeon2 gyro = new WPI_Pigeon2(0);
+  SwerveDriveOdometry swerveOdometry = new SwerveDriveOdometry(chassisConstants.swerveKinematics, getYaw());
+
+	public SwerveSubsystem() {
+		new Thread(() -> {
+			try {
+				Thread.sleep(1000);
+				zeroHeading();
+			} catch (Exception e) {
+			}
+		}).start();
+	}
+
+	public void zeroHeading() {
+		gyro.reset();
+	}
+
+	public double getHeading() {
+		return Math.IEEEremainder(gyro.getAngle(), 360);
+	}
+
+	public Rotation2d getRotation2d() {
+		return Rotation2d.fromDegrees(getHeading());
+	}
+
+	@Override
+	public void periodic() {
+		// This method will be called once per scheduler run
+		SmartDashboard.putNumber("Robot Heading", getHeading());
+		SmartDashboard.putNumber("FrontL Angle: ", frontLeft.getState().angle.getDegrees());
+		SmartDashboard.putNumber("FrontR Angle: ", frontRight.getState().angle.getDegrees());
+		SmartDashboard.putNumber("BackL Angle: ", backLeft.getState().angle.getDegrees());
+		SmartDashboard.putNumber("BackR Angle: ", backRight.getState().angle.getDegrees());
+
+	}
+	// public void stopModules(){
+	// frontLeft.stop();
+	// frontRight.stop();
+	// backLeft.stop();
+	// backRight.stop();
+	// }
+  
+  public void drive(Translation2d translation, double rotation, boolean fieldRelative) {
+    SwerveModuleState[] swerveModuleStates =
+        Constants.chassisConstants.swerveKinematics.toSwerveModuleStates(
+            fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                                translation.getX(), 
+                                translation.getY(), 
+                                rotation, 
+                                getYaw()
+                            )
+                            : new ChassisSpeeds(
+                                translation.getX(), 
+                                translation.getY(), 
+                                rotation)
+                            );
+    SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, chassisConstants.maxSpeedMPS);
+    frontLeft.setDesiredState(swerveModuleStates[0]);
+    frontRight.setDesiredState(swerveModuleStates[1]);
+    backLeft.setDesiredState(swerveModuleStates[2]);
+    backRight.setDesiredState(swerveModuleStates[3]);
+  }    
+  
+	public Rotation2d getYaw() {
+		double[] ypr = new double[3];
+		gyro.getYawPitchRoll(ypr);
+		return (chassisSetUp.invertedGyro) ? Rotation2d.fromDegrees(360 - ypr[0]) : Rotation2d.fromDegrees(ypr[0]);
+	}
+}
